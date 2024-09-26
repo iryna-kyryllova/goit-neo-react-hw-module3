4aa59d9fe71690a676fa7079335f73601bc7bfe8
Iryna Kyryllova <keira.kirillova@gmail.com>
2024-09-26 18:49:48 +0200

    add search filter to contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
+import SearchBox from 'components/SearchBox/SearchBox'
 import ContactList from 'components/ContactList/ContactList'
 import initialContacts from 'data/contacts'
 
 const App = () => {
   const [contacts, setContacts] = useState(initialContacts)
+  const [search, setSearch] = useState('')
+
+  const filteredContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(search.toLocaleLowerCase())
+  )
 
   return (
     <div className='container'>
       <h1>Phonebook</h1>
-      <ContactList contacts={contacts} />
+      <SearchBox search={search} setSearch={setSearch} />
+      <ContactList contacts={filteredContacts} />
     </div>
   )
 }
